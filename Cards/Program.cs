7b555033103a70accd0e6cd48c425c12720272e5
Matt-glitch-tech <65725733+Matt-glitch-tech@users.noreply.github.com>
2023-01-25 23:08:49 +0200

    Add project files.

---
+++ Cards/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // TODO Read input from text file
+        var line = "Player1:JS,2S,6C,3C,2H";
+        var winner = CalculateWinner(new List<string> { line });
+        //var A = Console.ReadLine();
+        Console.ReadLine();
+    }
+
+    private static string CalculateWinner(List<string> list)
+    {
+        foreach (var item in list)
+        {
+            var index = item.IndexOf(':');
+            var name = item.Substring(0, index-1);
+            // split the rest of the string starting at the index, split the rest of the sting from JS to 2H.
+            // split method to do the splitting, it will generate a string array.
+            char[] separator = { ',', ' ' };
+            string[] cards = item.Split((char)index);
+            string[] card = item.Split(separator);
+        }
+
+        throw new NotImplementedException();
+    }
+}
+
+public class PlayerHand
+{
+    public string Name { get; set; }
+    public List<string> Cards { get; set; }
+}
